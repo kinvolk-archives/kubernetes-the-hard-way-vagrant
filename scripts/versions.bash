64a189edb69bbf93ad8ddaf1089ec6a85a86cfc1
Michael Schubert <michael@kinvolk.io>
2018-01-16 16:45:42 +0100

    versions: update k8s + etcd + cri-o

--- scripts/versions.bash
+++ scripts/versions.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-readonly k8s_version="v1.8.2"
-readonly etcd_version="v3.2.9"
+readonly k8s_version="v1.9.2"
+readonly etcd_version="v3.2.15"
 readonly cfssl_version="R1.2"
 readonly traefik_version="v1.3.8"
+readonly crio_version="v1.9.0"
