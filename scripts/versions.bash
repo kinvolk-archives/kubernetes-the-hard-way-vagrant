8e51e4c84795d8a9dc3c460904e084710d24bddf
Slach <bloodjazman@gmail.com>
2019-01-19 11:29:02 +0100

    scripts/versions: update Kubernetes from v1.11.2 -> v1.12.5

--- scripts/versions.bash
+++ scripts/versions.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-readonly k8s_version="v1.11.2"
+# https://storage.googleapis.com/kubernetes-release/release/stable.txt
+readonly k8s_version="v1.12.5"
 readonly etcd_version="v3.3.9"
 readonly cfssl_version="R1.2"
 readonly traefik_version="v1.3.8"
