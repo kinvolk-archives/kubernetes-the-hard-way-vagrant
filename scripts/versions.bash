e1b1cb609ccb4b35e34f0a59a6a870e7234e75bc
RAJKUMAR NATARAJAN <raj.cs.psg@gmail.com>
2019-02-19 23:18:52 -0500

    updated trafeik crio and etcd versions

--- scripts/versions.bash
+++ scripts/versions.bash
@@ -2,7 +2,7 @@
 
 # https://storage.googleapis.com/kubernetes-release/release/stable.txt
 readonly k8s_version="v1.13.0"
-readonly etcd_version="v3.3.9"
-readonly cfssl_version="R1.2"
-readonly traefik_version="v1.3.8"
-readonly crio_version="v1.11.2"
+readonly etcd_version="v3.3.12"
+readonly cfssl_version="R1.3.2"
+readonly traefik_version="v1.7.9"
+readonly crio_version="v1.13.0"
