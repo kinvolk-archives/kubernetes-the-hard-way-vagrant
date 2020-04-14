fe9d2e164569cdd8b20f5425002e477f33bd038b
Suraj Deshmukh <suraj@kinvolk.io>
2020-04-14 10:34:14 +0530

    host setup: Fix DNS issue in Ubuntu
    Ubuntu has DNS issue with CNI, adding this commit makes sure that the
    right resolv.conf file is utilised.
    
    Signed-off-by: Suraj Deshmukh <suraj@kinvolk.io>

--- scripts/vagrant-setup-hosts-file.bash
+++ scripts/vagrant-setup-hosts-file.bash
@@ -12,3 +12,7 @@ cat <<EOF | sudo tee -a /etc/hosts
 192.168.199.21 worker-1
 192.168.199.22 worker-2
 EOF
+
+# This is added to get around the DNS issue in Ubuntu
+# See https://github.com/kubernetes/kubernetes/issues/66067
+sudo ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
