5fa32d88349eb9e4ddb6df837f09eef00b3f8ff3
Suraj Deshmukh <suraj@kinvolk.io>
2020-04-14 10:35:43 +0530

    versions.bash: bump up versions
    This commit bumps up version of all the tools used, also adds new
    variables for conmon binary used with crio, cni plugin version was
    hardcoded before, runc version is also parameterised now.
    
    Signed-off-by: Suraj Deshmukh <suraj@kinvolk.io>

--- scripts/versions.bash
+++ scripts/versions.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # https://storage.googleapis.com/kubernetes-release/release/stable.txt
-readonly k8s_version="v1.13.0"
-readonly etcd_version="v3.3.9"
+readonly k8s_version="v1.18.1"
+readonly etcd_version="v3.4.7"
 readonly cfssl_version="R1.2"
 readonly traefik_version="v1.3.8"
-readonly crio_version="v1.11.2"
+readonly crio_version="v1.17.2"
+readonly conmon_version="v2.0.15"
+readonly cni_plugins_version="v0.8.5"
+readonly runc_version="v1.0.0-rc10"
